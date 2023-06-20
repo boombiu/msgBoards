4e3f3799c8ef0bf340ae9feda3fc11b53130395e
Julio Zhang <1719457586@qq.com>
2023-06-20 15:24:21 +0800

    create project

---
+++ src/test/java/com/mb/msgboards/MsgBoardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mb.msgboards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgBoardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
